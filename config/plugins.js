3f5d1a644790e66269b472781cf8c71048dd5002
Saqib.Sattar <saqib.sattar@therightsw.com>
2022-10-13 11:57:38 +0500

    strapi-plugins.js file added

---
+++ config/plugins.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // ...
+    'strapi-plugin-fcm': {
+        enabled: true,
+        resolve: './node_modules/strapi-plugin-fcm' // path to plugin folder
+    },
+    // ...
+}
